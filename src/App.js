1af4ee9b2b3671c706b016a3a27d5b43de6e2bba
shuvo18103107 <18103107@iubat.edu>
2021-11-26 22:07:35 +0600

    use state deeep dive final

--- src/App.js
+++ src/App.js
@@ -1,37 +1,13 @@
 import React from 'react';
-import ClickCounter from './components/ClickCounter';
-import Counter from './components/Counter';
-import Section from './components/Section';
-import percel from './lib/Context';
-// eslint - disable - next - line;
+import Counter from './components/counter';
+import Todo from './components/Todo';
 
 class App extends React.Component {
-    state = {
-        // eslint-disable-next-line react/no-unused-state
-        theme: 'dark',
-        // eslint-disable-next-line react/no-unused-state
-        switchTheme: () => {
-            this.setState(({ theme }) => {
-                if (theme === 'dark') {
-                    return { theme: 'light' };
-                }
-
-                return { theme: 'dark' };
-            });
-        },
-    };
-
     render() {
         return (
             <div className="app">
-                <Counter>
-                    {(counter, incrementCounter) => (
-                        <ClickCounter count={counter} incrementCount={incrementCounter} />
-                    )}
-                </Counter>
-                <percel.Provider value={this.state}>
-                    <Section />
-                </percel.Provider>
+                <Todo />
+                <Counter />
             </div>
         );
     }
--- src/lib/Context.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-const percel = React.createContext();
-
-export default percel;
-// etar vitore ekhn provider r consumer 2 ta method ache obj akare createcontext class so amra oi obj acess kore oi method 2 ta pelam percel e
