167dd309142a019b00ef65275c477e4e43960611
shuvo18103107 <18103107@iubat.edu>
2021-11-27 09:21:49 +0600

    ✅ useeffect deep dive

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import React from 'react';
-import Counter from './components/counter';
-import Todo from './components/Todo';
+import { React, useState } from 'react';
+import MyComponent from './components/Mycomponent';
 
-class App extends React.Component {
-    render() {
-        return (
-            <div className="app">
-                <Todo />
-                <Counter />
-            </div>
-        );
-    }
-}
+const App = function () {
+    const [show, setShow] = useState(true);
+
+    return (
+        <div className="app">
+            <div>{show && <MyComponent />}</div>
+            <br />
+            <br />
+            <button type="button" onClick={() => setShow((prevstate) => !prevstate)}>
+                {show ? 'Hide Post' : 'Show Post'}
+            </button>
+        </div>
+    );
+};
 
 export default App;
+
+// ekhane jokhn amra timer ta on korci mycomponent e tokhn app theke button click e  umount hoye jaowar por o kintu timer er state ta change hocee background e. tai warning dicee Can't perform a React state update on an unmounted component, tai timer bondo na korle memeory leak hoye jete pare
+// eta class component e solved kora jai componentwillunmount diye , cg ekta component jokhn unmount hoi tokhn eta call hoi
