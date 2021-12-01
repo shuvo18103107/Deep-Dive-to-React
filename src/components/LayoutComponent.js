5bac80a0582884223a482475d0ca22801256f758
shuvo18103107 <18103107@iubat.edu>
2021-12-01 19:09:47 +0600

    ✅ custom hook

--- src/components/LayoutComponent.js
+++ src/components/LayoutComponent.js
@@ -2,7 +2,7 @@ import useWindowWidth from '../hooks/useWindowWidth';
 
 // eslint-disable-next-line react/function-component-definition
 export default function LayoutComponentOne() {
-    const onSmallScreen = useWindowWidth();
+    const onSmallScreen = useWindowWidth(768);
     return (
         <div>
             <h1>You are browsing on {onSmallScreen ? 'small' : 'large'} device</h1>
